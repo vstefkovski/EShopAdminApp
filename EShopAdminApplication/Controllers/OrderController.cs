632fbf72a8bdb48659f3c614786737fbc9c6f377
Vojdan Stefkovski <vojdan.stefkovski@students.finki.ukim.mk>
2022-11-13 19:46:47 +0100

    order info sent to mail update

--- EShopAdminApplication/Controllers/OrderController.cs
+++ EShopAdminApplication/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using EShopAdminApplication.Models;
+﻿using ClosedXML.Excel;
+using EShopAdminApplication.Models;
 using GemBox.Document;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -31,7 +32,7 @@ namespace EShopAdminApplication.Controllers
 
             return View(data);
         }
-        
+
         public IActionResult Details(Guid orderId)
         {
             HttpClient client = new HttpClient();
@@ -50,6 +51,52 @@ namespace EShopAdminApplication.Controllers
             var data = response.Content.ReadAsAsync<Order>().Result;
 
             return View(data);
+        }
+
+        [HttpGet]
+        public FileContentResult ExportAllOrders()
+        {
+            String fileName = "Orders.xlsx";
+            String contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            using (var workbook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet = workbook.Worksheets.Add("All Orders");
+                worksheet.Cell(1, 1).Value = "Order Id";
+                worksheet.Cell(1, 2).Value = "Costumer Email";
+
+
+                HttpClient client = new HttpClient();
+
+                String URL = "https://localhost:44344/api/Admin/GetAllActiveOrders";
+
+                HttpResponseMessage response = client.GetAsync(URL).Result;
+
+                var data = response.Content.ReadAsAsync<List<Order>>().Result;
+
+                for (int i = 1; i <= data.Count(); i++)
+                {
+                    var item = data[i - 1];
+
+                    worksheet.Cell(i + 1, 1).Value = item.Id.ToString();
+                    worksheet.Cell(i + 1, 2).Value = item.User.Email;
+
+                    for (int p = 1; p <= item.Products.Count(); p++)
+                    {
+                        worksheet.Cell(1, p + 2).Value = "Product-" + (p);
+                        worksheet.Cell(i + 1, p + 2).Value = item.Products.ElementAt(p-1).SelectedProduct.ProductName;
+                        
+                    }
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    return File(content, contentType, fileName);
+                }
+            }
         }
 
         public FileContentResult CreateInvoice(Guid orderId)
@@ -83,7 +130,7 @@ namespace EShopAdminApplication.Controllers
                 totalPrice += item.Quantity * item.SelectedProduct.ProductPrice;
             }
             document.Content.Replace("{{ProductList}}", sb.ToString());
-            document.Content.Replace("{{TotalPrice}}", totalPrice.ToString()+"$");
+            document.Content.Replace("{{TotalPrice}}", totalPrice.ToString() + "$");
 
             var stream = new MemoryStream();
             document.Save(stream, new PdfSaveOptions());
